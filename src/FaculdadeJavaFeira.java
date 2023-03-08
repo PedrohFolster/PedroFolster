f22c792c55008e8031fd80dce3b8fb1fc63044f6
pedro.santos13 <pedro.santos13@alunos.sc.senac.br>
2023-03-08 19:25:17 -0300

    .

--- src/FaculdadeJavaFeira.java
+++ src/FaculdadeJavaFeira.java
@@ -5,6 +5,6 @@ public class FaculdadeJavaFeira {
         double laranja = 5.5 * 3;
 
         System.out.println("Java foi a feira e comprou 1kg de maça de maça a 7,25 reais o quilo e 3 quilos de laranja a 5,5 reais, o quilo.");
-        System.out.println("O valor total é de: " + (maca + laranja) + ".");
+        System.out.println("O valor total gasto na feira foi de: " + (maca + laranja) + ".");
     }
 }
