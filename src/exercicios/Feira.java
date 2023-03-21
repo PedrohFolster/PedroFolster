7fe82928d436b6e5115f1f2db748fc8bb16d8d86
pedro.santos13 <pedro.santos13@alunos.sc.senac.br>
2023-03-20 21:45:09 -0300

    ..

--- src/exercicios/Feira.java
+++ src/exercicios/Feira.java
@@ -1,13 +1,40 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class Feira {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        double maca = 7.25;
-        double laranja = 5.5 * 3;
+        double quiloMaca;
+        double precoMaca;
+        double quiloLaranja;
+        double precoLaranja;
 
-        System.out.println("Java foi a feira e comprou 1kg de maça de maça a 7,25 reais o quilo e 3 quilos de laranja a 5,5 reais, o quilo.");
-        System.out.println("O valor total gasto na feira foi de: " + (maca + laranja) + ".");
+        System.out.println("Quantidade de quilos de Maça: ");
+        quiloMaca = scan.nextDouble();
+
+        System.out.println("Valor do preço do quilo da maça: ");
+        precoMaca = scan.nextDouble();
+
+        System.out.println("Quantidade de quilos de Laranja: ");
+        quiloLaranja = scan.nextDouble();
+
+        System.out.println("Valor do preço do quilo da laranja: ");
+        precoLaranja = scan.nextDouble();
+
+        System.out.println("Quilos de maça: " + quiloMaca);
+        System.out.println("Preço da maça: " + precoMaca);
+        System.out.println("Quilos de laranja: " + quiloLaranja);
+        System.out.println("Preço da laranja: " + precoLaranja);
+
+        double totalLaranja = (quiloLaranja * precoLaranja);
+        double totalMaca = (quiloMaca * precoMaca);
+
+        System.out.println("Valor total gasto na feira: R$ " + (totalLaranja + totalMaca));
+
+//        System.out.println("Java foi a feira e comprou 1kg de maça de maça a 7,25 reais o quilo e 3 quilos de laranja a 5,5 reais, o quilo.");
+//        System.out.println("O valor total gasto na feira foi de: " + (maca + laranja) + ".");
     }
 }
