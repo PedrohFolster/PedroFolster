128dd9c67d378906ff02759ad599f33589306dd2
pedro.santos13 <pedro.santos13@alunos.sc.senac.br>
2023-03-29 19:05:04 -0300

    ..

--- src/exercicios/Exercicio9.java
+++ src/exercicios/Exercicio9.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Exercicio9 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("Digite o modelo do carro: ");
-        String modelo = sc.nextLine();
+        String modelo = scan.nextLine();
 
         System.out.print("Digite o custo de fábrica do carro: ");
-        double custoFabrica = sc.nextDouble();
+        double custoFabrica = scan.nextDouble();
 
         double percentualDistribuidor = 0.28;
         double percentualImpostos = 0.45;
@@ -19,7 +19,5 @@ public class Exercicio9 {
 
         System.out.println("O modelo do carro é: " + modelo);
         System.out.println("O custo final do carro para o consumidor é: " + custoFinal);
-
-        sc.close();
     }
 }
