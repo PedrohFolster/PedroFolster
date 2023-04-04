a93fa1155f501c2fc65c832db160ec150d8537a3
PedroFolster <pedro.santos13@alunos.sc.senac.br>
2023-04-04 15:13:09 -0300

    .

---
+++ src/exercicios_condicionais/Exercicio9.java
@@ -0,0 +1,134 @@
+package exercicios_condicionais;
+
+import java.util.Scanner;
+
+public class Exercicio9 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int numeroEscolha;
+        int kgMaca;
+        int kgMorango;
+
+        System.out.println("Escolha entre as opções abaixo: \n" +
+                "1 - Maça\n" +
+                "2 - Morango\n" +
+                "3 - Ambas as frutas");
+
+        numeroEscolha = scan.nextInt();
+
+        switch (numeroEscolha) {
+            case 1: {
+                System.out.println("Você escolheu a categoria Maça.\n" +
+                        "Quantos quilos de maça você deseja comprar? ");
+                kgMaca = scan.nextInt();
+                if (kgMaca > 5) {
+                    if (kgMaca <= 8) {
+                        System.out.println("Você está comprando " + kgMaca + "Kg por isso estará recebendo um desconto.");
+                        double precoMacaAcimaDe5Kg = 1.5;
+                        double valorTotal = precoMacaAcimaDe5Kg * kgMaca;
+                        System.out.println("O valor total da sua compra foi de: " + valorTotal);
+                    }
+                }
+                if (kgMaca <= 5) {
+                    System.out.println("Você está comprando " + kgMaca + "Kg para ganhar o desconto, você precisa estar comprando acima de 8Kg para obter um desconto maior.");
+                    double precoMacaAbaixoDe5kg = 1.8;
+                    double valorTotal = precoMacaAbaixoDe5kg * kgMaca;
+                    System.out.println("O valor total da sua compra foi de: " + valorTotal);
+                }
+                if (kgMaca > 8) {
+                    System.out.println("Você está comprando " + kgMaca + "Kg por isso estará recebendo um desconto de 10%.");
+                    double precoMacaAcimaDe5Kg = 1.5;
+                    double valorTotal = (precoMacaAcimaDe5Kg * kgMaca);
+                    double valorDesconto = (precoMacaAcimaDe5Kg * kgMaca) * (0.1);
+                    System.out.println("O valor total da sua compra foi de: " + (valorTotal - valorDesconto));
+                }
+            }
+            break;
+            case 2: {
+                System.out.println("Você escolheu a categoria Morango.\n" +
+                        "Quantos quilos de morango você deseja comprar? ");
+                kgMorango = scan.nextInt();
+
+                if (kgMorango > 5) {
+                    if (kgMorango <= 8) {
+                        System.out.println("Você está comprando " + kgMorango + "Kg por isso estará recebendo um desconto.");
+
+                        double precoMorangoAcimaDe5Kg = 2.2;
+                        double valorTotalMaiorQue5Kg = precoMorangoAcimaDe5Kg * kgMorango;
+                        System.out.println("O valor total da sua compra foi de: " + valorTotalMaiorQue5Kg);
+                    }
+                }
+                if (kgMorango > 8) {
+                    System.out.println("Você está comprando " + kgMorango + "Kg por isso estará recebendo um desconto de 10%.");
+                    double precoMorangoAcimaDe5Kg = 2.2;
+                    double valorTotal = (precoMorangoAcimaDe5Kg * kgMorango);
+                    double valorDesconto = (precoMorangoAcimaDe5Kg * kgMorango) * (0.1);
+                    System.out.println("O valor total da sua compra foi de: " + (valorTotal - valorDesconto));
+                }
+                if (kgMorango <= 5) {
+                    System.out.println("Você está comprando " + kgMorango + "Kg para ganhar o desconto, você precisa estar comprando acima de 8Kg para obter um desconto maior.");
+                    double precoMorangoAbaixoDe5kg = 2.5;
+                    double valorTotalAbaixoDe5Kg = precoMorangoAbaixoDe5kg * kgMorango;
+                    System.out.println("O valor total da sua compra foi de: " + valorTotalAbaixoDe5Kg);
+                }
+            }
+            break;
+            case 3: {
+                System.out.println("Quantos quilos de maça você deseja comprar? ");
+                kgMaca = scan.nextInt();
+
+                if (kgMaca > 5) {
+                    if (kgMaca <= 8) {
+                        System.out.println("Você está comprando " + kgMaca + "Kg por isso estará recebendo um desconto.");
+                        double precoMacaAcimaDe5Kg = 1.5;
+                        double valorTotal = precoMacaAcimaDe5Kg * kgMaca;
+                        System.out.println("O valor total da sua compra foi de: " + valorTotal);
+                    }
+                }
+                if (kgMaca <= 5) {
+                    System.out.println("Você está comprando " + kgMaca + "Kg para ganhar o desconto, você precisa estar comprando acima de 8Kg para obter um desconto maior.");
+                    double precoMacaAbaixoDe5kg = 1.8;
+                    double valorTotal = precoMacaAbaixoDe5kg * kgMaca;
+                    System.out.println("O valor total da sua compra foi de: " + valorTotal);
+                }
+                if (kgMaca > 8) {
+                    System.out.println("Você está comprando " + kgMaca + "Kg por isso estará recebendo um desconto de 10%.");
+                    double precoMacaAcimaDe5Kg = 1.5;
+                    double valorTotal = (precoMacaAcimaDe5Kg * kgMaca);
+                    double valorDesconto = (precoMacaAcimaDe5Kg * kgMaca) * (0.1);
+                    System.out.println("O valor total da sua compra foi de: " + (valorTotal - valorDesconto));
+                }
+
+                System.out.println("Quantos quilos de Morango você deseja comprar? ");
+                kgMorango = scan.nextInt();
+
+                if (kgMorango > 5) {
+                    if (kgMorango <= 8) {
+                        System.out.println("Você está comprando " + kgMorango + "Kg por isso estará recebendo um desconto.");
+
+                        double precoMorangoAcimaDe5Kg = 2.2;
+                        double valorTotalMaiorQue5Kg = precoMorangoAcimaDe5Kg * kgMorango;
+                        System.out.println("O valor total da sua compra foi de: " + valorTotalMaiorQue5Kg);
+                    }
+                }
+                if (kgMorango > 8) {
+                    System.out.println("Você está comprando " + kgMorango + "Kg por isso estará recebendo um desconto de 10%.");
+                    double precoMorangoAcimaDe5Kg = 2.2;
+                    double valorTotal = (precoMorangoAcimaDe5Kg * kgMorango);
+                    double valorDesconto = (precoMorangoAcimaDe5Kg * kgMorango) * (0.1);
+                    System.out.println("O valor total da sua compra foi de: " + (valorTotal - valorDesconto));
+                }
+                if (kgMorango <= 5) {
+                    System.out.println("Você está comprando " + kgMorango + "Kg para ganhar o desconto, você precisa estar comprando acima de 8Kg para obter um desconto maior.");
+                    double precoMorangoAbaixoDe5kg = 2.5;
+                    double valorTotalAbaixoDe5Kg = precoMorangoAbaixoDe5kg * kgMorango;
+                    System.out.println("O valor total da sua compra foi de: " + valorTotalAbaixoDe5Kg);
+                }
+            }
+            break;
+        }
+    }
+}
