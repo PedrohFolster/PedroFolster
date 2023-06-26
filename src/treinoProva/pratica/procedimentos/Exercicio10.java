4e78ec1ad5c3860e286a74dc3ccc705103b919a4
PedroFolster <pedro.santos13@alunos.sc.senac.br>
2023-06-26 01:38:10 -0300

    .

--- src/treinoProva/pratica/procedimentos/Exercicio10.java
+++ src/treinoProva/pratica/procedimentos/Exercicio10.java
@@ -1,28 +1,30 @@
-package strings;
+package treinoProva.pratica.procedimentos;
 
 import java.util.Scanner;
 
 public class Exercicio10 {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
 
+        String [] numeroPorExtenso = {"ZERO", "UM", "DOIS", "TRÊS", "QUATRO", "CINCO", "SEIS", "SETE", "OITO", "NOVE"};
         int numero;
-        String[] extenso = {"ZERO", "UM", "DOIS", "TRÊS", "QUATRO", "CINCO", "SEIS", "SETE", "OITO", "NOVE"};
 
-        System.out.print("Informe um número: ");
+        System.out.print("Informe um número para obter o mesmo por extenso: ");
         numero = scan.nextInt();
 
         String numeroString = String.valueOf(numero);
 
         for (int i = 0; i < numeroString.length(); i++) {
             int digito = numeroString.charAt(i) - '0';
-            System.out.print(extenso[digito]);
+            System.out.print(numeroPorExtenso[digito]);
 
             if (i < numeroString.length() - 1) {
                 System.out.print(", ");
             }
         }
-
         System.out.println();
+
     }
 }
